534086a54a06df40c38874c94546059ca140c665
DediData <33499600+Dedi-Data@users.noreply.github.com>
2024-04-20 17:08:41 +0330

    standards

--- woocommerce.php
+++ woocommerce.php
@@ -1,28 +1,43 @@
 <?php
 /**
  * The woocommerce template file
+ *
+ * @package Free_Template
  */
+
+declare(strict_types=1);
+
+get_header();
 ?>
- 
-<?php get_header(); ?>
 <main id="main" class="site-main">
-	<div class="container"><?php
-		if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
-		<div class="row"><?php
-		} ?>
-			<div id="primary" class="site-content content-area col-xs-12<?php if( is_active_sidebar( 'sidebar-1' ) ) { echo ' pull-right col-sm-9'; } ?>">
+	<div class="container">
+		<?php
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			?>
+		<div class="row">
+			<?php
+		}
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			$sidebar_class = ' pull-right col-sm-9';
+		}
+		?>
+			<div id="primary" class="site-content content-area col-xs-12<?php echo esc_attr( $sidebar_class ); ?>">
 				<div class="wc-content panel box">
 					<?php woocommerce_content(); ?>
 				</div>
-			</div><?php
+			</div>
+			<?php
 
 			if ( is_active_sidebar( 'sidebar-1' ) ) {
 				get_sidebar();
 			}
 
-		if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+			if ( is_active_sidebar( 'sidebar-1' ) ) {
+				?>
 		</div>
-		<?php } ?>
+				<?php
+			}
+			?>
 	</div>
 </main><!-- .site-main -->
 <?php

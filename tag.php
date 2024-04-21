5326a1df404477906f43fcffaa97eecdf8f274b0
DediData <33499600+Dedi-Data@users.noreply.github.com>
2024-04-21 13:12:16 +0330

    standards

--- tag.php
+++ tag.php
@@ -1,21 +1,35 @@
 <?php
 /**
  * The main template file
+ * 
+ * @package Free_Template
  */
-?>
- 
-<?php get_header(); ?>
+
+declare(strict_types=1);
+
+get_header(); ?>
 <main id="main" class="site-main">
-	<div class="container"><?php
-		if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
-		<div class="row"><?php
-		} ?>
-			<div id="primary" class="site-content content-area col-xs-12<?php if( is_active_sidebar( 'sidebar-1' ) ) { echo ' pull-right col-sm-9'; } ?>"><?php
-			
+	<div class="container">
+		<?php
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			?>
+		<div class="row">
+			<?php
+		}
+		
+		if ( is_active_sidebar( 'sidebar-1' ) ) { 
+			$sidebar_class = ' pull-right col-sm-9';
+		}
+
+		?>
+			<div id="primary" class="site-content content-area col-xs-12<?php echo esc_attr( $sidebar_class ); ?>">
+			<?php
 			dynamic_sidebar( 'content-top' );
 			
-			if(is_tag() && tag_description()){ ?>
-				<div class="box panel text-justify"><?php echo tag_description(); ?></div><?php
+			if ( is_tag() && tag_description() ) {
+				?>
+				<div class="box panel text-justify"><?php echo tag_description(); ?></div>
+				<?php
 			}
 
 			if ( have_posts() ) {
@@ -23,6 +37,7 @@
 				/* Start the Loop */
 				while ( have_posts() ) {
 					the_post();
+
 					/*
 					 * Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
@@ -31,28 +46,33 @@
 					get_template_part( 'template-parts/post/content', get_post_format() );
 				}
 
-				Free_Template::posts_pagination( array(
-					'prev_text' => '<span>' . esc_html__( 'Previous', 'free-template' ) . '</span>',
-					'next_text' => '<span>' . esc_html__( 'Next', 'free-template' ) . '</span>',
-					'type'						=> 'list',
-					'end_size'					=> 3,
-					'mid_size'					=> 3,
-				) );
-
-			}
+				Free_Template::posts_pagination(
+					array(
+						'prev_text' => '<span>' . esc_html__( 'Previous', 'free-template' ) . '</span>',
+						'next_text' => '<span>' . esc_html__( 'Next', 'free-template' ) . '</span>',
+						'type'      => 'list',
+						'end_size'  => 3,
+						'mid_size'  => 3,
+					)
+				);
+			}//end if
 			
 			dynamic_sidebar( 'content-bottom' );
 			
 			?>
-			</div><?php
+			</div>
+			<?php
 
 			if ( is_active_sidebar( 'sidebar-1' ) ) {
 				get_sidebar();
 			}
 
-		if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+			if ( is_active_sidebar( 'sidebar-1' ) ) {
+				?>
 		</div>
-		<?php } ?>
+				<?php
+			}
+			?>
 	</div>
 </main><!-- .site-main -->
 <?php
--- template-fullwidth.php
+++ template-fullwidth.php
@@ -3,48 +3,53 @@
  * The template for displaying full width pages.
  *
  * Template Name: Full width
+ * 
+ * @package Free_Template
  */
-?>
- 
-<?php get_header(); ?>
+
+declare(strict_types=1);
+
+get_header(); ?>
 <main id="main" class="site-main">
 	<div class="container">
-		<div id="primary" class="site-content content-area col-xs-12"><?php
-
-		if(is_home() or is_front_page()){
-			dynamic_sidebar( 'frontend-content-top' );
-		}
-		dynamic_sidebar( 'content-top' );
+		<div id="primary" class="site-content content-area col-xs-12">
+			<?php
+			if ( is_home() || is_front_page() ) {
+				dynamic_sidebar( 'frontend-content-top' );
+			}
+			dynamic_sidebar( 'content-top' );
 		
-		if ( have_posts() ) {
+			if ( have_posts() ) {
 
-			/* Start the Loop */
-			while ( have_posts() ) {
-				the_post();
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/post/content', get_post_format() );
+				/* Start the Loop */
+				while ( have_posts() ) {
+					the_post();
+
+					/*
+					* Include the Post-Format-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/post/content', get_post_format() );
+				}
+
+				Free_Template::posts_pagination(
+					array(
+						'prev_text' => '<span>' . esc_html__( 'Previous', 'free-template' ) . '</span>',
+						'next_text' => '<span>' . esc_html__( 'Next', 'free-template' ) . '</span>',
+						'type'      => 'list',
+						'end_size'  => 3,
+						'mid_size'  => 3,
+					)
+				);
+			}//end if
+
+			if ( is_home() || is_front_page() ) {
+				dynamic_sidebar( 'frontend-content-bottom' );
 			}
 
-			Free_Template::posts_pagination( array(
-				'prev_text' => '<span>' . esc_html__( 'Previous', 'free-template' ) . '</span>',
-				'next_text' => '<span>' . esc_html__( 'Next', 'free-template' ) . '</span>',
-				'type'						=> 'list',
-				'end_size'					=> 3,
-				'mid_size'					=> 3,
-			) );
-
-		}
-
-		if(is_home() or is_front_page()){
-			dynamic_sidebar( 'frontend-content-bottom' );
-		}
-		dynamic_sidebar( 'content-bottom' );
-		
-		?>
+			dynamic_sidebar( 'content-bottom' );
+			?>
 		</div>
 	</div>
 </main><!-- .site-main -->

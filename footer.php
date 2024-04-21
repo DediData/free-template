c1bf4a4b7fdd9e539af5ec32fc6dae856a03b954
DediData <33499600+Dedi-Data@users.noreply.github.com>
2024-04-21 18:35:53 +0330

    update footer.php

--- footer.php
+++ footer.php
@@ -4,8 +4,12 @@
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
+ * @package Free_Template
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
+
+declare(strict_types=1);
+
 ?>
 	<footer class="page-footer navbar-inverse text-warning">
 		<div class="container">
@@ -19,15 +23,21 @@
 		<?php get_template_part( 'template-parts/part/nav-bottom' ); ?>
 		<div class="footer-bottom">
 			<div class="container">
-				<?php if(is_front_page()){ ?>
+				<?php if ( is_front_page() ) { ?>
 				<div class="pull-left col-xs-12 col-sm-6">
-					<a href="<?php echo esc_url( __('https://dedidata.com', 'free-template') ); ?>" title="<?php esc_attr_e('Free Theme by DediData', 'free-template'); ?>" target="_blank">
-						<?php esc_attr_e('Design by DediData', 'free-template'); ?>
+					<a href="<?php echo esc_url( esc_attr__( 'https://dedidata.com', 'free-template' ) ); ?>" title="<?php esc_attr_e( 'Free Theme by DediData', 'free-template' ); ?>" target="_blank">
+						<?php esc_attr_e( 'Design by DediData', 'free-template' ); ?>
 					</a>
 				</div>
 				<?php } ?>
 				<div class="pull-right col-xs-12 col-sm-6">
-					<p><?php echo esc_html( __('Copyright', 'free-template') . ' &copy; ' . date('Y ') . get_bloginfo( 'name' ) . '. ' .  __('All right reserved.', 'free-template') ); ?></p>
+					<p><?php echo esc_html__( 'Copyright', 'free-template' ) . esc_html( ' &copy; ' . gmdate( 'Y ' ) . get_bloginfo( 'name' ) . '. ' ) . esc_html__( 'All right reserved.', 'free-template' ); ?></p>
+					<p>
+						<?php
+						// Translators: %1$s is current year and %2$s is site name
+						printf( esc_html__( 'Copyright &copy; %1$s %2$s. All right reserved.', 'free-template' ), esc_html( gmdate( 'Y' ) ), esc_html( get_bloginfo( 'name' ) ) );
+						?>
+					</p>
 				</div>
 			</div>
 		</div>
